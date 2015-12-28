cdcb4950ba9baf19d7006bd65e04498b36a00f01
perugoal1 <peru_goal1@yahoo.com>
2015-12-29 00:18:53 +0530

    Added angularjs in homepage

--- public/js/home.js
+++ public/js/home.js
@@ -7,7 +7,8 @@ app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRo
 	$stateProvider
 	.state("home",{
 		url:"/home",
-		templateUrl :"/partials/initial.html"
+		templateUrl :"/partials/initial.html",
+		controller:'intitialCtrl'
 	})
 	.state("catList",{
 		url:'/catList',
@@ -17,4 +18,58 @@ app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRo
 		url:'/tutorials',
 		templateUrl:"/partials/tutDetails.html"
 	})
+}]);
+
+
+app.factory('model',function(){
+	
+	modelVar ={
+		lists : [{id:1,
+				 mainheading:"Blog1",
+				 desc:"Description of the awesome app 1",
+				 mainbg:"",
+				 tags:["html"],
+				 date:{date:"",value:"1451221619320"},
+				 content:[{seqno:1,type:"heading",desc:"hey"},
+				          {seqno:2,type:"para",desc:"this is a para"},
+				          {seqno:1,type:"code",desc:"this is a code"},
+				          {seqno:1,type:"img",desc:"this is an img",src:""}],
+				 prev:{id:"2"}
+			     },
+			     {id:2,
+					 mainheading:"Blog2",
+					 desc:"Description of the awesome app 1",
+					 mainbg:"",
+					 tags:["css"],
+					 date:{date:"",value:"1451221619321"},
+					 content:[{seqno:1,type:"heading",desc:"hey"},
+					          {seqno:2,type:"para",desc:"this is a para"},
+					          {seqno:1,type:"code",desc:"this is a code"},
+					          {seqno:1,type:"img",desc:"this is an img",src:""}]
+				     },
+			     {id:3,
+					 mainheading:"Blog3",
+					 desc:"Description of the awesome app 1",
+					 mainbg:"",
+					 tags:["html","css"],
+					 date:{date:"",value:"1451221619322"},
+					 content:[{seqno:1,type:"heading",desc:"hey"},
+					          {seqno:2,type:"para",desc:"this is a para"},
+					          {seqno:1,type:"code",desc:"this is a code"},
+					          {seqno:1,type:"img",desc:"this is an img",src:""}]
+				     }
+		         ],
+		         
+		  category:['HTML','CSS','Javascript']
+	};
+	
+	return modelVar;
+});
+
+app.controller('intitialCtrl',['$scope','model',function($scope,model){
+	$scope.models = model.lists;
+	$scope.categories = model.category;
+	$scope.loadList = function(category){
+		$scope.catFilter = category;
+	};
 }]);
