fdc4ef2ee4d64ea9c53f5c21b131c66ec83ba2a1
perugoal1 <peru_goal1@yahoo.com>
2016-07-22 00:51:49 +0530

    db config changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 // config.host = 'mongodb://localhost/devtutz';
-config.host = 'mongodb://heroku_ps8m91lt:goalpower@ds045464.mlab.com:45464/heroku_ps8m91lt'
+config.host = 'mongodb://heroku_ps8m91lt:ak0lbh56qmargim6m29l7l5mj5@ds045464.mongolab.com:45464/heroku_ps8m91lt'
 config.port = 27017;
 
 module.exports = config;
